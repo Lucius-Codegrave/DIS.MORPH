fd6f9dfcf340d0721597d2442e0798f9e566bad2
Lucius-Codegrave <luciuscodegrave@gmail.com>
2025-08-12 19:32:05 +0200

    chore(deploy): setup GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import svelteConfig from './svelte.config';
+import { sveltePreprocess } from 'svelte-preprocess';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [svelte(svelteConfig)],
+  plugins: [
+    svelte({
+      preprocess: sveltePreprocess(),
+    }),
+  ],
   assetsInclude: ['**/*.glsl', '**/*.vert', '**/*.frag'],
   resolve: {
     alias: {
       src: path.resolve(__dirname, 'src'),
     },
   },
+  base: '/DIS.MORPH/',
 });
